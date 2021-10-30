e46649980bf4ba8cf9b8a0748775c3b606c6615d
Raul Singh <raulpsingh2@gmail.com>
2021-10-30 18:02:51 +0300

    Update Login.dart

--- individual_project/lib/Pages/Login.dart
+++ individual_project/lib/Pages/Login.dart
@@ -8,6 +8,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class  _LoginPageState extends State<LoginPage> {
+  TextEditingController _emailController = TextEditingController();  
+  TextEditingController _passwordController = TextEditingController();  
+  
+  String _email;
+  String _password;
+  bool showLogin = true;
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +29,94 @@ class  _LoginPageState extends State<LoginPage> {
       );
 
     }
+
+      Widget _input(Icon icon, String hint, TextEditingController controller, bool obscure){
+        return Container(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: TextField(
+            controller: controller,
+            obscureText: obscure,
+            style: TextStyle(fontSize: 20, color: Colors.white),
+            decoration: InputDecoration(
+              hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white30),
+              hintText: hint,
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 3)
+                ),
+                enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white54, width: 1)
+            ),
+            prefixIcon: Padding(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: IconTheme(
+                data: IconThemeData(color: Colors.white),
+                child: icon,
+              ),
+            )
+          ),
+        ),
+        );
+
+
+      }
+
+      Widget _button(String text, void func()){
+        return RaisedButton(
+          splashColor: Theme.of(context).primaryColor,
+          highlightColor: Theme.of(context).primaryColor ,
+          color: Colors.white,
+          child: Text(
+            text,
+            style: TextStyle( fontWeight: FontWeight.bold, color: Theme.of(context).primaryColor, fontSize: 20),
+          ),
+          onPressed: (){
+            func();
+          },
+        
+        
+        
+        );
+      }
+
+
+      Widget _form(String label, void func()){
+        return Container(
+          child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(bottom: 20, top: 100),
+              child: _input(Icon(Icons.email), "Email", _emailController, false)
+            ),
+             Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: _input(Icon(Icons.password), "Password", _passwordController, true)
+            ),
+            SizedBox(height: 20,),
+            Padding(
+              padding: EdgeInsets.only(left: 20,right: 20),
+              child: Container(
+                height: 50, 
+                width: MediaQuery.of(context).size.width,
+                child: _button(label,func),
+              ),
+            )
+          ],  
+          )
+          );
+      }
+
+      void _loginUser(){
+        _email= _emailController
+      }
+
+
+
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: Column(
         children: <Widget>[
           _logo(),
-          //_form(),
+          _form('LOGIN',(){}),
 
 
         ],
